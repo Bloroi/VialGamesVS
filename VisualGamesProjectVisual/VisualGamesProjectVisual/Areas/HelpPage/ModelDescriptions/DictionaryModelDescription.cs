9d4b09c453dab39a0e9c3399a1f77c7950ed102c
Alexandre <alex.10101997@gmail.com>
2017-11-08 14:48:10 +0100

    Premier commit

---
+++ VisualGamesProjectVisual/VisualGamesProjectVisual/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VisualGamesProjectVisual.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ VisualGamesProjectVisual/VisualGamesProjectVisual/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VisualGamesProjectVisual.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
