2bd40c4e4ac1685acf78c7032617c9463227e866
Alexandre <alexandre.adriaens@gmail.com>
2017-11-18 17:04:06 +0100

    dsfdsgdsfsd

--- VisualGamesProjectVisual/VisualGamesProjectVisual/Models/Database.cs
+++ VisualGamesProjectVisual/VisualGamesProjectVisual/Models/Database.cs
@@ -8,7 +8,7 @@ namespace VialGamesVisual.Models
 {
 	public class Database
 	{
-		public static readonly string CONNECTION_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		public static readonly string CONNECTION_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDVialGames;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         //VICTOR//Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
         //ALEX//Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDVialGames;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
         public static SqlConnection GetConnection()
