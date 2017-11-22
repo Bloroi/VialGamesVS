02387812a2ee7d06d245a2d7c7af2b2a0b553453
Victor <victor.papleux@gmail.com>
2017-11-22 12:33:45 +0100

    22/11 12:33

--- VisualGamesProjectVisual/VisualGamesProjectVisual/Controllers/TokenController.cs
+++ VisualGamesProjectVisual/VisualGamesProjectVisual/Controllers/TokenController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Net;
+﻿using System.Net;
 using System.Web.Http;
-using JWT.Filters;
+using VisualGamesProjectVisual.Filters;
 
 namespace VisualGamesProjectVisual.Controllers
 {
