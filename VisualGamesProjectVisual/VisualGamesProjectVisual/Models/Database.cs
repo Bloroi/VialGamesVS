b5a70c6d3c18588ccd0ef28f23f2196dff7ed6e6
Alexandre <alexandre.adriaens@gmail.com>
2017-12-08 21:48:01 +0100

    mini modifs

--- VisualGamesProjectVisual/VisualGamesProjectVisual/Models/Database.cs
+++ VisualGamesProjectVisual/VisualGamesProjectVisual/Models/Database.cs
@@ -9,8 +9,10 @@ namespace VialGamesVisual.Models
 	public class Database
 	{
 		//public static readonly string CONNECTION_STRING = @"Data Source=servervialgames.database.windows.net;Initial Catalog = vialgamesDB; Integrated Security = False; User ID = vialgames; Password=AlexVictor123;Connect Timeout = 30; Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        public static readonly string CONNECTION_STRING = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = DbProjectTI; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        public static SqlConnection GetConnection()
+		//public static readonly string CONNECTION_STRING = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = DbProjectTI; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		public static readonly string CONNECTION_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDVialGames;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+		public static SqlConnection GetConnection()
 		{
 			return new SqlConnection(Database.CONNECTION_STRING);
 		}
